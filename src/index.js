d1a3ad9fe6398cf43243ccd702b629085b0c6e88
Adyaapatil09 <you@example.com>
2023-09-12 16:39:00 +0530

    editied index.js links

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 // Function to fetch and display listings
 function fetchAndDisplayListings() {
-  fetch("show_listings.php")
+  // Fetch listings data from the server using "display.php"
+  fetch("display.php")
     .then((response) => response.json())
     .then((data) => {
+      // Get the listings container element
       const listingsContainer = document.getElementById("listings");
 
-      // Clear existing listings
+      // Clear existing listings in the container
       listingsContainer.innerHTML = "";
 
-      // Loop through listings and display them
+      // Loop through the fetched listings data and display each listing
       data.forEach((listing) => {
+        // Create a new HTML element for the listing
         const listingElement = document.createElement("div");
         listingElement.classList.add("produce");
         listingElement.innerHTML = `
-          <img src="${listing.image}" alt="${listing.name}" />
+          <img
+            src="${listing.image}"
+            alt="${listing.name}"
+          />
+          <button class="btn danger-btn" onclick="removeListing('${listing.name}')">Remove</button>
           <h2>${listing.name}</h2>
-          <p>${listing.quantity} KG</p>
+          <p>${listing.quantity}KG</p>
           <p>₹${listing.price}</p>
           <div class="buttons">
-            <button class="btn primary-btn-outline" onclick="buyListing('${listing.name}')">Buy Now</button>
-            <button class="btn primary-btn-outline" onclick="addToCart('${listing.name}')">Add to Cart</button>
-            <button class="btn danger-btn" onclick="removeListing('${listing.name}')">Remove</button>
+            <a onclick="buyListing('${listing.name}')" class="btn primary-btn-outline"> Buy Now </a>
+            <a onclick="addToCart('${listing.name}')"  class="btn primary-btn-outline"> add to cart </a>
           </div>
         `;
+
+        // Add the listing element to the listings container
         listingsContainer.appendChild(listingElement);
       });
     })
@@ -31,66 +39,37 @@ function fetchAndDisplayListings() {
     });
 }
 
-// Function to add a new listing
-function addListing() {
-  const name = document.getElementById("name").value;
-  const quantity = document.getElementById("quantity").value;
-  const price = document.getElementById("price").value;
-  const image = document.getElementById("image").value;
-
-  const newListing = {
-    name,
-    quantity,
-    price,
-    image,
-  };
-
-  fetch("add_listing.php", {
-    method: "POST",
-    body: JSON.stringify(newListing),
-  })
-    .then((response) => response.json())
-    .then(() => {
-      fetchAndDisplayListings();
-      // Clear form fields after adding
-      document.getElementById("name").value = "";
-      document.getElementById("quantity").value = "";
-      document.getElementById("price").value = "";
-      document.getElementById("image").value = "";
-    })
-    .catch((error) => {
-      console.error("Error adding listing:", error);
-    });
-}
-
 // Function to remove a listing
-function removeListing(name) {
-  fetch("remove_listing.php", {
+function removeListing(xyz) {
+  // Send a request to "remove.php" to remove the listing with the specified "xyz" identifier
+  fetch("remove.php", {
     method: "POST",
-    body: JSON.stringify({ name }),
+    body: JSON.stringify({ xyz: xyz }),
   })
     .then((response) => response.json())
     .then(() => {
+      // After successfully removing the listing, refresh the listings on the page
       fetchAndDisplayListings();
     })
     .catch((error) => {
-      console.error("Error removing listing:", error);
+      console.error("Warning removing listing:", error);
     });
 
-    
+  // Refresh the DOM immediately (before the response is received)
+  fetchAndDisplayListings();
 }
 
-// Function to handle buying a listing
+// Function to handle buying a listing (to be implemented)
 function buyListing(name) {
   // Implement buy functionality here
   alert(`You bought ${name}`);
 }
 
-// Function to handle adding to cart
+// Function to handle adding to cart (to be implemented)
 function addToCart(name) {
   // Implement add to cart functionality here
   alert(`Added ${name} to cart`);
 }
 
-// Initialize by fetching and displaying listings
+// Initialize by fetching and displaying listings when the page loads
 fetchAndDisplayListings();
